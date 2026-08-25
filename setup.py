c378346c6695c373640b5bf74e9f7b2f87341ff7
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2026-08-25 14:11:15 +0200

    Update to version 4.2.0

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 exec(open("pvml/version.py").read())
 
 setup(
-    name = "pvml",
-    version = __version__,
-    description = "Processor Validation Management Layer",
-    url = "http://github.com/stcorp/pvml",
-    author = "S[&]T",
-    license = "BSD",
+    name="pvml",
+    version=__version__,
+    description="Processor Validation Management Layer",
+    url="http://github.com/stcorp/pvml",
+    author="S[&]T",
+    license="BSD",
     packages=["pvml"],
     entry_points={"console_scripts": ["pvml = pvml.__main__:main"]},
     install_requires=["lxml"],
